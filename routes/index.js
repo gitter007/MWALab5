dbf6a2ab4d7107974689eec1e0045f9dbd83e35d
gitter007 <erkashine@yahoo.com>
2017-07-01 22:18:21 -0500

    code working

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+/*********************************************** 
+  CS572 - MODERN WEB APPLICATION PROGRAMMING
+  MUM
+  Student: #985803
+  Lecture 5 / Exercise 
+************************************************/
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'MWA #985803' });
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,30 @@
+/*********************************************** 
+  CS572 - MODERN WEB APPLICATION PROGRAMMING
+  MUM
+  Student: #985803
+  Lecture 5 / Exercise 
+************************************************/
+var express = require('express');
+var router = express.Router();
+var fetch = require('node-fetch');
+let url ='http://jsonplaceholder.typicode.com/users/';
+
+var data = '';
+
+fetch(url)
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        data = json;
+    });
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  //console.log(typeof usersJSON);
+    res.render('users.ejs', { title: 'MWA #985803', items:data});
+
+});
+
+module.exports = router;
