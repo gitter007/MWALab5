142f2d7d06be92d7284f0bff1ab10f9b2350d2bb
gitter007 <erkashine@yahoo.com>
2017-07-01 23:59:29 -0500

    Promise way final

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ var fetch = require('node-fetch');
 var Rx = require('@reactivex/rxjs');
 let url ='http://jsonplaceholder.typicode.com/users/';
 
-var data = '';
+var data = {};
 
 //Promise way
 var fetching = fetch(url); //return Promise
@@ -31,8 +31,53 @@ callPromise();
 
 //Reactive Programming(Observables) way
 
-//Async Wait way
+// function dataProcess(f) {
+//     f.buffer().then(function(res) 
+//     {
+//         return res.toString();
+//     })
+//     .then(function(json) {
+//         data = JSON.parse(json);
+//     })
+//     .catch((error) =>
+//         {new Error("Error during processing");}
+//         );
+// }
+// Rx.Observable.fromPromise(fetch(url))
+//     .subscribe(
+//         (res)=> {
+//                 dataProcess(res);
+//                 //console.log(res.buffer().then());
+//         }
+//         ,
+//         (error)=> {
+//             console.log(error);
+//         }
+//         ,
+//         () => console.log('Reactive Completed'));        
 
+
+//Async Wait way
+// async function callAsync() {
+//     try{
+//         console.log('Before reading data Async Await');
+//         let results = await(fetch(url));
+//         console.log('After Async Await');
+//        return results.json();
+//     }
+//     catch(error){
+//         console.log(error.message);
+//     }
+// }
+
+// var data = {};
+// callAsync()    
+//     .then(function(json) {
+//         data = json;
+//     });
+
+
+//Routing
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
   //console.log(typeof usersJSON);
